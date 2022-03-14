7aa74467787061306f624f6c05ea18c1dfbb638e
vimiomori <vivian.muchen@gmail.com>
2022-03-14 17:59:27 +0900

    chore: refactor

--- main.go
+++ main.go
@@ -9,27 +9,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// the questions to ask
-// var qs = []*survey.Question{
-//     {
-//         Name:     "name",
-//         Prompt:   &survey.Input{Message: "What is your name?"},
-//         Validate: survey.Required,
-//         Transform: survey.Title,
-//     },
-//     {
-//         Name: "color",
-//         Prompt: &survey.Select{
-//             Message: "Choose a color:",
-//             Options: []string{"red", "blue", "green"},
-//             Default: "red",
-//         },
-//     },
-//     {
-//         Name: "age",
-//         Prompt:   &survey.Input{Message: "How old are you?"},
-//     },
-// }
 
 func main() {
 	dirName := ""
